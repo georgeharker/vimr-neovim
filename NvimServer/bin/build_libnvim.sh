f75d47364d64ad4703fe26ad281cdec46f62e09d
Tae Won Ha <h@taewon.de>
2022-02-13 11:44:34 +0100

    Remove unnec scripts

--- NvimServer/bin/build_libnvim.sh
+++ NvimServer/bin/build_libnvim.sh
@@ -2,36 +2,13 @@
 set -Eeuo pipefail
 
 readonly clean=${clean:?"true or false"}
-readonly download_gettext=${download_gettext:?"true or false"}
 declare target; target="$(uname -m)"; readonly target
-
-download_gettext() {
-  local tag; tag=$(cat "./NvimServer/Resources/nvimserver-deps-for-download.txt"); readonly tag
-  local -r archive_folder="./NvimServer/build"
-  rm -rf "${archive_folder}" && mkdir -p ${archive_folder}
-
-  local -r archive_file_name="gettext-${target}.tar.bz2"
-  local -r archive_file_path="${archive_folder}/${archive_file_name}"
-
-  curl -o "${archive_file_path}" -L "https://github.com/qvacua/neovim/releases/download/${tag}/gettext-${target}.tar.bz2"
-
-  pushd "${archive_folder}" >/dev/null
-    tar xf "${archive_file_name}"
-  popd >/dev/null
-
-  local -r third_party_folder="./NvimServer/third-party"
-  rm -rf "${third_party_folder}" && mkdir -p "${third_party_folder}"
-
-  mv "${archive_folder}/lib" "${third_party_folder}"
-  mv "${archive_folder}/include" "${third_party_folder}"
-}
 
 build_libnvim() {
   local -r deployment_target=$1
 
   # Brew's gettext does not get sym-linked to PATH
   export PATH="/opt/homebrew/opt/gettext/bin:/usr/local/opt/gettext/bin:${PATH}"
-  ln -sf ./NvimServer/local.mk local.mk
 
   make \
     SDKROOT="$(xcrun --show-sdk-path)" \
@@ -45,10 +22,6 @@ build_libnvim() {
 main() {
   # This script is located in /NvimServer/bin and we have to go to /
   pushd "$(dirname "${BASH_SOURCE[0]}")/../.." >/dev/null
-
-  if [[ "${download_gettext}" == true ]]; then
-    download_gettext
-  fi
 
   echo "### Building libnvim"
   local deployment_target
