16444d8623adba9ea90842928e66f9193668e0af
Tae Won Ha <h@taewon.de>
2022-06-26 10:45:23 +0200

    Add build script for cross compiling

--- NvimServer/bin/build_libnvim.sh
+++ NvimServer/bin/build_libnvim.sh
@@ -2,7 +2,6 @@
 set -Eeuo pipefail
 
 readonly clean=${clean:?"true or false"}
-declare target; target="$(uname -m)"; readonly target
 
 build_libnvim() {
   local -r deployment_target=$1
@@ -10,12 +9,15 @@ build_libnvim() {
   # Brew's gettext does not get sym-linked to PATH
   export PATH="/opt/homebrew/opt/gettext/bin:/usr/local/opt/gettext/bin:${PATH}"
 
+  macos_flags="-DCMAKE_OSX_DEPLOYMENT_TARGET=${deployment_target} -DCMAKE_OSX_ARCHITECTURES=arm64\;x86_64"
+
+  # W/o setting MACOSX_DEPLOYMENT_TARGET, the dependencies have min. macOS set to the macOS you're on.
   make \
+    CMAKE_BUILD_TYPE=Release \
     SDKROOT="$(xcrun --show-sdk-path)" \
     MACOSX_DEPLOYMENT_TARGET="${deployment_target}" \
-    CMAKE_EXTRA_FLAGS="-DCMAKE_OSX_DEPLOYMENT_TARGET=${deployment_target}" \
-    DEPS_CMAKE_FLAGS="-DCMAKE_OSX_DEPLOYMENT_TARGET=${deployment_target} -DCMAKE_CXX_COMPILER=$(xcrun -find c++)" \
-    CMAKE_FLAGS="-DENABLE_LIBINTL=OFF -DCUSTOM_UI=1 -DFEAT_TUI=0 -DCMAKE_BUILD_TYPE=Release" \
+    CMAKE_EXTRA_FLAGS="-DENABLE_LIBINTL=OFF -DCUSTOM_UI=1 -DFEAT_TUI=0 ${macos_flags}" \
+    DEPS_CMAKE_FLAGS="${macos_flags}" \
     libnvim
 }
 
@@ -25,7 +27,7 @@ main() {
 
   echo "### Building libnvim"
   local deployment_target
-  deployment_target=$(cat "./NvimServer/Resources/${target}_deployment_target.txt")
+  deployment_target=$(cat "./NvimServer/Resources/macos_deployment_target.txt")
   readonly deployment_target
 
   if [[ "${clean}" == true ]]; then
@@ -39,3 +41,4 @@ main() {
 }
 
 main
+
--- NvimServer/bin/build_nvimserver.sh
+++ NvimServer/bin/build_nvimserver.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -Eeuo pipefail
 
-declare -x target; target="$(uname -m)"; readonly target
 declare -r -x clean=${clean:?"if true, will clean libnvim and nvimserver"}
 readonly build_libnvim=${build_libnvim:?"true or false"}
 readonly build_dir=${build_dir:-"./.build"}
@@ -9,8 +8,8 @@ readonly build_dir=${build_dir:-"./.build"}
 package() {
   local -r build_folder_path=$1
   pushd "${build_folder_path}" >/dev/null
-    tar cjf "NvimServer-${target}.tar.bz2" NvimServer
-    echo "Packaged NvimServer for ${target} to $(realpath "NvimServer-${target}.tar.bz2")"
+    tar cjf "NvimServer.tar.bz2" NvimServer
+    echo "Packaged universal NvimServer to $(realpath "NvimServer.tar.bz2")"
   popd >/dev/null
 }
 
@@ -18,17 +17,17 @@ main() {
   echo "### Building NvimServer"
   # This script is located in /NvimServer/bin and we have to go to /
   pushd "$(dirname "${BASH_SOURCE[0]}")/../.." >/dev/null
-    if [[ "${build_libnvim}" == true ]]; then
-      ./NvimServer/bin/build_libnvim.sh
-    fi
-
     if [[ "${clean}" == true ]]; then
       rm -rf "${build_dir}"
     fi
 
-    swift build -c release --product NvimServer
+    if [[ "${build_libnvim}" == true ]]; then
+      ./NvimServer/bin/build_libnvim.sh
+    fi
 
-    package "${build_dir}/release"
+    swift build --arch arm64 --arch x86_64 -c release --product NvimServer
+
+    package "${build_dir}/apple/Products/Release"
 
   popd >/dev/null
   echo "### Built NvimServer"
