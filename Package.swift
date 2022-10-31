1484f47db20539e14b7c37545df4e05c6d1e7a67
Tae Won Ha <h@taewon.de>
2022-10-31 20:31:16 +0100

    Bump deployment target for x86 to Catalina
    The minimum bottle version of gettext brew offers is Catalina now.

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 
 let package = Package(
   name: "NvimServer",
-  platforms: [.macOS(.v10_13)],
+  platforms: [.macOS(.v10_15)],
   products: [
     .library(name: "NvimServerTypes", targets: ["NvimServerTypes"]),
     .library(name: "RxNeovim", targets: ["RxNeovim"]),
